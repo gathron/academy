ec80d278a56faf44df8f301233eb4e25d8bd9355
Sylwia Curzytek <sylwia.curzytek@openx.com>
2022-03-01 21:05:30 +0100

    string add

--- string_operations.py
+++ string_operations.py
@@ -7,6 +7,7 @@ def list_from_string(long_string):
 def main():
     long_string = "To jest testowy String taki jak najdłuższy, bla bla bla bla"
     print(list_from_string(long_string))
+    s = "{'muffin' : 'lolz', 'foo' : 'kitty'}"
 
 
 if __name__ == "__main__":
