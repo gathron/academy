423d8c23889d04b2b17adf9798e7bf9c5fa761a3
Aleksander Skworzec <aleksander.skworzec@gmail.com>
2022-03-01 19:57:06 +0100

    list added

--- tuple_lists.py
+++ tuple_lists.py
@@ -5,8 +5,16 @@ def get_tuple():
     return tuple1
 
 
+def get_list_reversed(my_range, my_step):
+    list1 = []
+    for x in range(my_range, 0, -my_step):
+        list1.append(x)
+    return list1
+
+
 def main():
     print(get_tuple())
+    print(get_list_reversed(10, 2))
 
 
 if __name__ == "__main__":
