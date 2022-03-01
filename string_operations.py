8cca8126ceee2f46cff97737f6e45949d49a8e9e
Aleksander Skworzec <aleksander.skworzec@gmail.com>
2022-03-01 22:12:17 +0100

    Modification

--- string_operations.py
+++ string_operations.py
@@ -1,13 +1,17 @@
-
-
 def list_from_string(long_string):
     return long_string.split(" ")
+
+
+def list_from_letters(long_string):
+    return [letter for letter in long_string if letter != " "]
 
 
 def main():
     long_string = "To jest testowy String taki jak najdłuższy, bla bla bla bla"
     print(list_from_string(long_string))
     s = "{'muffin' : 'lolz', 'foo' : 'kitty'}"
+    print(eval(s))
+    print(list_from_letters(long_string))
 
 
 if __name__ == "__main__":
